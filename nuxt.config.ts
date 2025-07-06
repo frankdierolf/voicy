35ec73c44a449af6c9d122f70a44459f00f94d67
Frank Dierolf <frank_dierolf@web.de>
2025-07-06 08:27:17 +0000

    feat: enable Nuxt 4 compatibility mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui-pro',
